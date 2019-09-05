16f329357ad9e2ae41661f625d80a5017ec7aa91
krill <lpmaze@gmail.com>
2019-09-05 15:07:59 -0400

    added jwt auth

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,23 +1,46 @@
 import React, { Component } from 'react'
 import { Button, Input } from '../Utils/Utils'
 import TokenService from '../../services/token-service'
+import AuthApiService from '../../services/auth-api-service'
 
 export default class LoginForm extends Component {
   static defaultProps = {
-    onLoginSuccess: () => { }
+    onLoginSuccess: () => { 
+      // do successful login stuff
+    }
   }
 
   state = { error: null }
 
-  handleSubmitBasicAuth = ev => {
+  // handleSubmitBasicAuth = ev => {
+  //   ev.preventDefault()
+  //   const { user_name, password } = ev.target
+
+  //   TokenService.saveAuthToken(TokenService.makeBasicAuthToken(user_name.value, password.value))
+
+  //   user_name.value = ''
+  //   password.value = ''
+  //   this.props.onLoginSuccess()
+  // }
+
+  handleSubmitJwtAuth = ev => {
     ev.preventDefault()
+    this.setState({error: null})
     const { user_name, password } = ev.target
 
-    TokenService.saveAuthToken(TokenService.makeBasicAuthToken(user_name.value, password.value))
-
-    user_name.value = ''
-    password.value = ''
-    this.props.onLoginSuccess()
+    AuthApiService.postLogin({
+      user_name: user_name.value,
+      password: password.value,
+    })
+    .then(res => {
+      user_name.value = ''
+      password.value = ''
+      TokenService.saveAuthToken(res.authToken)
+      this.props.onLoginSuccess()
+    }).catch(res => {
+      this.setState({error: res.error})
+    })
+    
   }
 
   render() {
@@ -25,7 +48,7 @@ export default class LoginForm extends Component {
     return (
       <form
         className='LoginForm'
-        onSubmit={this.handleSubmitBasicAuth}
+        onSubmit={this.handleSubmitJwtAuth}
       >
         <div role='alert'>
           {error && <p className='red'>{error}</p>}
